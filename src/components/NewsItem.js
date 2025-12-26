5cf908f8341540a91710ffa3da6bf2d454544b5a
Arman Shaikh <22071120@ycce.in>
2025-12-27 00:39:19 +0530

    Corrected cards components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,9 +7,17 @@ export class NewsItem extends Component {
   }
 
   render() {
+    let {title, description , imgurl , newsUrl} = this.props
     return (
-      <div>
-        i will chage once it will change all components
+      <div className="my-3">
+      <div class="card" style={{width: "18rem"}}>
+        <img src={imgurl} class="card-img-top" alt="..."/>
+        <div class="card-body">
+          <h5 class="card-title">{title}...</h5>
+          <p class="card-text">{description}...</p>
+          <a href={newsUrl} target="_blank" class="btn btn-sn btn-primary">Read more</a>
+        </div>
+      </div>
       </div>
     )
   }
